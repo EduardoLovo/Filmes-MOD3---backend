5ec7ce5ed697ad368254813ebfd17745b99fab59
EduardoLovo <eduardo.lovo@gmail.com>
2021-12-16 22:15:08 -0300

    index atualizado

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 
 // cors para integração com front
 const cors = require('cors');
 
-const corsOptions = {
-    origin: 'http://localhost:3001',
-    optionsSuccessStatus: 200,
-}
+const Conn = require('./models/conn/conn');
+
+const filmesRouter = require('./routers/filmes.routes');
 
 // ==============================================
 
 const app = express();
 
 app.use(express.json());  // Faz as reqs do express trabalhar em json
-app.use(cors(corsOptions));
+app.use(cors());
+
 
 // ==============================================
 
+// Rotas ===================
+
+app.use('/filmes', filmesRouter);
+
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo');
+});
+
+// ==============================================
+
+
 //Conexão local com mongodb
-const Conn = require('./models/conn/conn');
 
 const db_url = process.env.DB_URL;
 const db_user = process.env.DB_USER;
@@ -28,26 +42,18 @@ const db_data = process.env.DB_DATA;
 
 Conn(db_url, db_user, db_pass, db_data)
 
+
+
+
+
+
+
+
+// ==============================================
+
 const port = 3000;
 
-// ==============================================
-
-
-// Rotas ===================
-const filmesRouter = require('./routers/filmes.routes');
-app.use('/filmes', filmesRouter);
-
-
-app.get('/', (req, res) => {
-    res.send('Bem vindo');
-});
-
-
-
-// ==============================================
-
-
 app.listen(process.env.PORT || port, () => {
-    console.info(`Rodando na porta http://localhost:${port}/`);
+    console.log(`Rodando na porta http://localhost:${port}`);
 });
 
